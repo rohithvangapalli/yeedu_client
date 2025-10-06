88bf7ab6aad4489210cc38e689ac0489de0c7414
rohithvangapalli <rohith.vangapalli@modak.com>
2025-10-06 15:07:31 +0530

    Basic job submit

--- client/jobs.py
+++ client/jobs.py
@@ -3,10 +3,11 @@ from .base import YeeduClient
 class JobsManager(YeeduClient):
 
     def create_job(self, job_data: dict):
-        return self._request("POST", "/jobs/create", json=job_data)
+        return self._request("POST", "/workspace/533/spark/job", json=job_data)
 
-    def run_job(self, job_id: int):
-        return self._request("POST", f"/jobs/{job_id}/run")
+    def run_job(self, run_data: dict):
+        return self._request("POST", "/workspace/533/spark/job/run", json=run_data)
+
 
     def get_job_status(self, job_id: int):
         return self._request("GET", f"/jobs/{job_id}/status")
